ca2987fd83bd91f85ca70e5acb0ac55751c220e6
Kittiphop22 <156067699+Kittiphop22@users.noreply.github.com>
2024-11-05 16:41:02 +0700

    Add router
    Add router

---
+++ portfolio/src/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <div style={{textAlign:"center"}}>About Us</div>
+    )
+}
--- portfolio/src/components/navbar/navbar.js
+++ portfolio/src/components/navbar/navbar.js
@@ -1,15 +1,25 @@
-import React,{useState} from 'react';
- import './navbar.css'
+import React from 'react';
+import { BrowserRouter,Link,Route,Routes } from 'react-router-dom';
+import './navbar.css'
+import Home from '../../home';
+import About from '../../about';
+import Contact from '../../contact';
 
  export default function Navbar(){
     return (
-        <nav className="navbar">
-            <ul className="navbar-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
+        <BrowserRouter>
+            <nav className="navbar">
+                <ul className="navbar-links">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>                
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/contact' element={<Contact/>} />
+        </Routes>    
+        </BrowserRouter>    
     );
 }
